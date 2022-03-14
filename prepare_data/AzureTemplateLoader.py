0c2abb7c12be6aa9a28503ad8f0e288bfaae6b9c
eugene.chuvyrov <eugene.chuvyrov@servicenow.com>
2022-03-14 07:53:30 +0100

    Final

--- prepare_data/AzureTemplateLoader.py
+++ prepare_data/AzureTemplateLoader.py
@@ -13,12 +13,16 @@ class AzureTemplateLoader():
         @param path_to_arm_templates (string): Path to ARM templates
                 """
         template_count = 0
+        available_templates = 0
         prompt_prefix = "\n\n###\n\n"
         prompt_suffix = "\n\n===\n\n"
         completion_prefix = " "
         completion_suffix = " END"
-
+        dict_original_infra = {}
+        
         for file_name in glob.iglob(path_to_arm_templates + '/**/metadata.json', recursive=True):
+            available_templates += 1
+
             # extract Summary and (later) Description from metadata - this will be our prompt
             metadata_file = open(file_name)
             metadata_json = json.load(metadata_file)
@@ -29,7 +33,6 @@ class AzureTemplateLoader():
             resource_start = file_name.lower().find("microsoft")
             resource_end = file_name.lower().find("/", resource_start)
             resource_name = file_name[resource_start: resource_end]
-
 
             if len(resource_name) > 0:
                 azure_deploy_path = file_name.replace("metadata", "azuredeploy")
@@ -49,9 +52,10 @@ class AzureTemplateLoader():
                             arm_template_string = arm_template_string + str(resource).lower()
 
                     completion_arm = completion_prefix + arm_template_string + completion_suffix
-
-                    # every 50th template will be our test set
-                    if template_count % 50 == 0:
+                    dict_original_infra[resource_name] = metadata_summary
+                    
+                    # every 10th template will be our test set
+                    if template_count % 10 == 0:
                         data_line = { "prompt" : metadata_summary, "truth": completion_arm }
                         data_line_string = json.dumps(data_line) + "\n"
                         training_file = open("test.json", "a")
@@ -68,4 +72,59 @@ class AzureTemplateLoader():
                     training_file.close()
                     template_count += 1
 
-        print("total files " + str(template_count))
+        print("total files " + str(template_count) + ", available files = " + str(available_templates))
+        self.get_more_data(path_to_arm_templates, dict_original_infra)
+
+    def get_more_data(self, path_to_arm_templates, dict_original_infra):
+        # in pure template definitions, there just isn't enough data for training
+        #  so use a bit of creativity here: since templates are very likely to contain elements not of just main type
+        #   i.e., Microsoft.Compute/VM, but also Microsoft.Networking/network, etc. get those elemets as additional 
+        #   "syndicated" data into the dataset
+        prompt_prefix = "\n\n###\n\n"
+        prompt_suffix = "\n\n===\n\n"
+        completion_prefix = " "
+        completion_suffix = " END"
+        additional_resources = 0
+        
+        print("\n\n========GETTING MORE DATA======")
+        for file_name in glob.iglob(path_to_arm_templates + '/**/azuredeploy.json', recursive=True):
+            # the resource we are looking for is Microsoft.* which should be a part of the path (filename)
+            file_resource_start = file_name.lower().find("microsoft")
+            file_resource_end = file_name.lower().find("/", file_resource_start)
+            file_resource_name = file_name[file_resource_start: file_resource_end]
+
+            for first_pass_resource in dict_original_infra:
+                # skip test folder
+                if file_name.find("/test/") < 0:
+                    if first_pass_resource != file_resource_name:
+                        print(file_name)
+                        arm_template_file = open(file_name)
+                        arm_template_json =  json.load(arm_template_file)
+                        arm_template_file.close()
+                        arm_template_string = ""
+
+                        all_resources = arm_template_json['resources']
+                        for resource in all_resources:
+                            if first_pass_resource.lower() in str(resource['type']).lower():
+                                arm_template_string = arm_template_string + str(resource).lower()
+                                completion_arm = completion_prefix + arm_template_string + completion_suffix
+
+                                # every 10th template will be our test set
+                                if additional_resources % 10 == 0:
+                                    data_line = { "prompt" : dict_original_infra[first_pass_resource], "truth": completion_arm }
+                                    data_line_string = json.dumps(data_line) + "\n"
+                                    training_file = open("test.json", "a")
+                                    write_it = training_file.write(data_line_string)
+                                    
+                                else:
+                                    # goal is to generate file for fine-tuning in the form
+                                    #  {"prompt": "<prompt text>", "completion": "<ideal generated text>"}
+                                    data_line = { "prompt" : dict_original_infra[first_pass_resource], "completion": completion_arm }
+                                    data_line_string = json.dumps(data_line) + "\n"
+                                    training_file = open("train.json", "a")
+                                    write_it = training_file.write(data_line_string)
+                                
+                                training_file.close()
+                                additional_resources += 1
+
+        print("total additional resources found " + str(additional_resources))
--- prepare_data/OpenAITests.py
+++ prepare_data/OpenAITests.py
@@ -2,8 +2,13 @@ import glob
 import os.path
 import json
 import nltk
-
+import urllib
+import urllib.request
+import requests
+import subprocess
 from nltk.translate.bleu_score import sentence_bleu
+import logging
+import openai
 
 class OpenAITests():
     """ Run tests on OpenAI:
@@ -12,6 +17,8 @@ class OpenAITests():
     """
     def __init__(self, test_file_path):
         completion_suffix = " END"
+        prompt_prefix = "\n\n###\n\n"
+        prompt_suffix = "\n\n===\n\n"
         
         test_file = open(test_file_path, 'r')
         test_file_lines = test_file.readlines()
@@ -19,8 +26,53 @@ class OpenAITests():
         for test_file_line in test_file_lines:
             test_file_line_json = json.loads(test_file_line)
 
-            print("Prompt: ", test_file_line_json['prompt'])
-            open_ai_output = input("Output of OpenAI API: ")
+            # print("Prompt: ", test_file_line_json['prompt'])
+            # open_ai_output = input("Output of OpenAI API: ")
+
+            # clean up prompt data
+            prompt = test_file_line_json['prompt'].replace(prompt_prefix, "").replace(prompt_suffix, "")
+
+            # import http.client as http_client
+            # http_client.HTTPConnection.debuglevel = 1
+
+            # # You must initialize logging, otherwise you'll not see debug output.
+            # logging.basicConfig()
+            # logging.getLogger().setLevel(logging.DEBUG)
+            # requests_log = logging.getLogger("requests.packages.urllib3")
+            # requests_log.setLevel(logging.DEBUG)
+            # requests_log.propagate = True
+            
+            # url = "https://api.openai.com/v1/engines/davinci:ft-personal-2022-02-26-15-30-06/completions"
+            
+            # headers = {
+            #             'Authorization': 'Bearer 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',
+            #             'Content-type': 'application/json'
+            #           }
+            
+            # payload = {
+            #             'prompt': prompt,
+            #             'model': 'davinci:ft-personal-2022-02-26-15-30-06', 
+            #             'temperature': 0, 
+            #             'max_tokens': 1025
+            #           }
+
+            # print(payload)
+            # response = requests.post(url, headers = headers, data = payload)
+            # print(response.text)
+            obj_open_ai_output = openai.Completion.create(
+                model = 'ada:ft-personal-2022-03-13-02-09-40',
+                temperature = 0,
+                max_tokens = 1025,
+                prompt = prompt)
+
+            open_ai_output = str(obj_open_ai_output.choices[0].text)
+            print("Output "  + open_ai_output)
+            # trim output at "END" delimiter - filler tokens should be trimmed out
+            if open_ai_output.find(completion_suffix) > 0:
+                open_ai_output = open_ai_output[:open_ai_output.find(completion_suffix)]
+    
+            print("Trimmed Output: " + open_ai_output)
+
             open_ai_output = open_ai_output + completion_suffix
             data_line = { "prompt" : test_file_line_json['prompt'], "truth": test_file_line_json['truth'], "openai_output": open_ai_output}
 
